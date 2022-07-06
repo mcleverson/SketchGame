d526ad219e7c02f7a4bf036c01c54f844174d579
mcleverson <mcleverson@hotmail.com>
2022-07-05 22:16:49 -0300

    Character walking and jumping

---
+++ Assets/scripts/HeroController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+
+    public float jumpForce = 30f;
+
+    Animator HeroAnimator;
+    Rigidbody HeroRB;
+    bool IsOnGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HeroAnimator = GetComponent<Animator>();
+        HeroRB = GetComponent<Rigidbody>();
+        IsOnGround = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) ||
+            Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow);
+
+
+        if ( Input.GetKeyDown(KeyCode.Space) && IsOnGround )
+        {
+            HeroAnimator.SetTrigger("Jump_Trig");
+            HeroAnimator.SetBool("jumping", true);
+            HeroRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            IsOnGround = false;
+        }
+        else
+        {
+            HeroAnimator.SetBool("walking", isWalking && IsOnGround);
+            HeroAnimator.SetBool("Jumping", false);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsOnGround = collision.gameObject.CompareTag("Ground");
+        HeroAnimator.SetBool("walking", IsOnGround);
+    }
+}
---
+++ Assets/scripts/ThirdPersonMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    public Transform cam;
+
+    float turnSmoothVelocity;
+
+    private void Start()
+    {
+        //Fix Character Controller 
+        controller.center = new Vector3(0, 10f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if ( direction.magnitude >= 0.1f )
+        {
+            float TargetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, TargetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+        }
+    }
+}
